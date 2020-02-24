a79b537580e35361f1f502ecef0c1cdb294b2ed6
Arash Thr <arashThr@outlook.com>
2020-02-24 22:14:54 +0800

    Ep4

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,5 +44,5 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
   
-let deadline = new Date('Feb 19, 2020 21:00:00')
+let deadline = new Date('Feb 26, 2020 21:00:00')
 initializeClock('clockdiv', deadline);
